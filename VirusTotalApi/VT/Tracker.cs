1a5b37e8f83caef4a40005bd6848f3804db120ae
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-13 14:42:40 +0200

    Added two structs representing small API objects

---
+++ VirusTotalApi/VT/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VirusTotalLib.VT
+{
+    /// <summary>
+    /// Represents a site tracker
+    /// </summary>
+    internal struct Tracker
+    {
+        /// <summary>
+        /// Id of the tracker
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Location of the tracker
+        /// </summary>
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+    /// <inheritdoc cref="Tracker"/>
+    public interface ITracker
+    {
+        /// <inheritdoc cref="Tracker.Id"/>
+        string Id { get; }
+        /// <inheritdoc cref="Tracker.Url"/>
+        string Url { get; }
+    }
+}
---
+++ VirusTotalApi/VT/Votes.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VirusTotalLib.VT
+{
+    /// <summary>
+    /// Represents Votes in VirusTotal community
+    /// </summary>
+    internal struct Votes : IVotes
+    {
+        /// <summary>
+        /// Positive votes
+        /// </summary>
+        [JsonPropertyName("harmless")]
+        public int Harmless { get; set; }
+        /// <summary>
+        /// Negative votes
+        /// </summary>
+        [JsonPropertyName("malicious")]
+        public int Malicious { get; set; }
+    }
+    /// <inheritdoc cref="Votes"/>
+    public interface IVotes
+    {
+        /// <inheritdoc cref="Votes.Harmless"/>
+        int Harmless { get; }
+        /// <inheritdoc cref="Votes.Malicious"/>
+        int Malicious { get; }
+    }
+}
