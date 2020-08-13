95195837ea7918f7bc3e0ca3ff698ecc3c37750c
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-13 13:42:24 +0200

    Implemented date of the analysis

---
+++ VirusTotalApi/AV/AnalysisDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace VirusTotalApi.AV
+{
+    internal class AnalysisDate
+    {
+        [JsonPropertyName("date")]
+        public DateTimeOffset Date { get; set; }
+    }
+    public interface IAnalysisDate
+    {
+        DateTimeOffset Date { get; }
+    }
+}
