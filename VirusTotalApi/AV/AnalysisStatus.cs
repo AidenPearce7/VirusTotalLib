91984b035dc450ce7012c949c65d1ef0f8339bf6
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-13 13:41:59 +0200

    Implemented status of the analysis

---
+++ VirusTotalApi/AV/AnalysisStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace VirusTotalApi.AV
+{
+    internal class AnalysisStatus : IAnalysisStatus
+    {
+        [JsonPropertyName("status")]
+        public IAnalysisStatus.Statuses Status { get; set; }
+    }
+    public interface IAnalysisStatus
+    {
+        [Flags]
+        enum Statuses
+        {
+            Completed = 1,
+            Queued = 2,
+            InProgress = 4
+        }
+        Statuses Status { get; }
+    }
+
+
+}
