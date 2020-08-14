f56f35bae22b00b2c3184b2483ef1dd2ac629bf4
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-14 18:09:11 +0200

    Added method overrides

--- VirusTotalApi/VT/Votes.cs
+++ VirusTotalApi/VT/Votes.cs
@@ -17,6 +17,25 @@ namespace VirusTotalLib.VT
         /// </summary>
         [JsonPropertyName("malicious")]
         public int Malicious { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Votes)) return false;
+            Votes votes = (Votes)obj;
+            if (votes.Harmless != Harmless
+                || votes.Malicious != Malicious) return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
     /// <inheritdoc cref="Votes"/>
     public interface IVotes
