a9080b7a381765d43434dbc71f4f418c015d94c9
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-14 18:13:49 +0200

    Relocated and renamed Url class, made an interface for read only access, created method overrides.

---
+++ VirusTotalApi/Url/IUrl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirusTotalLib.Url
+{
+    /// <inheritdoc cref="Url"/>
+    public interface IUrl
+    {
+        /// <inheritdoc cref="Url.Categories"/>
+        public IReadOnlyDictionary<string, string> Categories { get; }
+        /// <inheritdoc cref="Url.FirstSubmissionDate"/>
+        public DateTimeOffset FirstSubmissionDate { get; }
+        /// <inheritdoc cref="Url.HtmlMeta"/>
+        public IReadOnlyDictionary<string, string[]> HtmlMeta { get; }
+        /// <inheritdoc cref="Url.LastAnalysisDate"/>
+        public DateTimeOffset LastAnalysisDate { get; }
+        /// <inheritdoc cref="Url.LastAnalysisResults"/>
+        public IReadOnlyDictionary<string, AV.IUrlAnalysisResult> LastAnalysisResults { get; }
+        /// <inheritdoc cref="Url.Statistics"/>
+        public AV.IUrlAnalysisStatistics Statistics { get; }
+        /// <inheritdoc cref="Url.LastFinalUrl"/>
+        public string LastFinalUrl { get; }
+        /// <inheritdoc cref="Url.LastHttpResponseCode"/>
+        public int LastHttpResponseCode { get; }
+        /// <inheritdoc cref="Url.LastHttpResponseContentLength"/>
+        public int LastHttpResponseContentLength { get; }
+        /// <inheritdoc cref="Url.LastHttpResponseContentSha256"/>
+        public string LastHttpResponseContentSha256 { get; }
+        /// <inheritdoc cref="Url.LastHttpResponseCookies"/>
+        public IReadOnlyDictionary<string, string> LastHttpResponseCookies { get; }
+        /// <inheritdoc cref="Url.LastHttpResponseHeaders"/>
+        public IReadOnlyDictionary<string, string> LastHttpResponseHeaders { get; }
+        /// <inheritdoc cref="Url.LastModificationDate"/>
+        public DateTimeOffset LastModificationDate { get; }
+        /// <inheritdoc cref="Url.LastSubmissionDate"/>
+        public DateTimeOffset LastSubmissionDate { get; }
+        /// <inheritdoc cref="Url.links"/>
+        public List<string> OutgoingLinks { get; }
+        /// <inheritdoc cref="Url.Reputation"/>
+        public int Reputation { get; }
+        /// <inheritdoc cref="Url.Tags"/>
+        public List<string> Tags { get; }
+        /// <inheritdoc cref="Url.TargetedBrand"/>
+        public IReadOnlyDictionary<string, string> TargetedBrand { get; }
+        /// <inheritdoc cref="Url.TimesSubmitted"/>
+        public int TimesSubmitted { get; }
+        /// <inheritdoc cref="Url.Title"/>
+        public string Title { get; }
+        /// <inheritdoc cref="Url.TotalVotes"/>
+        public VT.IVotes TotalVotes { get; }
+        /// <inheritdoc cref="Url.Trackers"/>
+        public IReadOnlyDictionary<string, VT.ITracker[]> Trackers { get; }
+        /// <inheritdoc cref="Url.OrignialUrl"/>
+        public string OrignialUrl { get; }
+
+    }
+    /// <inheritdoc cref="PremiumUrl"/>
+    public interface IPremiumUrl : IUrl
+    {
+        [Flags]
+        public enum FaviconEnum
+        {
+            /// <summary>
+            /// Difference hash
+            /// </summary>
+            dhash,
+            /// <summary>
+            /// favicons MD5 hash
+            /// </summary>
+            raw_md5
+        }
+        /// <inheritdoc cref="PremiumUrl.Favicon"/>
+        IReadOnlyDictionary<FaviconEnum, string> Favicon { get; }
+    }
+}
---
+++ VirusTotalApi/Url/Url.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using VirusTotalLib.AV;
+using VirusTotalLib.VT;
+
+namespace VirusTotalLib.Url
+{
+    internal class Url : IUrl
+    {
+        /// <summary>
+        /// Key is the partner that categorised the URL, value is the category.
+        /// </summary>
+        [JsonPropertyName("categories")]
+        public Dictionary<string, string> Categories { get; set; }
+
+
+        /// <summary>
+        /// Date where the URL was first submitted to VirusTotal
+        /// </summary>
+        [JsonPropertyName("first_submission_date")]
+        [JsonConverter(typeof(Converters.DateTimeOffsetConverter))]
+        public DateTimeOffset FirstSubmissionDate { get; set; }
+
+        /// <summary>
+        /// Contains all meta tags
+        /// </summary>
+        [JsonPropertyName("html_meta")]
+        public Dictionary<string, string[]> HtmlMeta { get; set; }
+
+        /// <summary>
+        /// Represents last time the Url was scanned
+        /// </summary>
+        [JsonPropertyName("last_analysis_date")]
+        [JsonConverter(typeof(Converters.DateTimeOffsetConverter))]
+        public DateTimeOffset LastAnalysisDate { get; set; }
+        
+        /// <summary>
+        /// Result from URl scanners, scanner name is key
+        /// </summary>
+        [JsonPropertyName("last_analysis_results")]
+        [JsonConverter(typeof(Converters.DictAnalysisResult))]
+        public Dictionary<string, AV.IUrlAnalysisResult> LastAnalysisResults { get; set; }
+        /// <summary>
+        /// Number of different results from this scans
+        /// </summary>
+        [JsonPropertyName("last_analysis_stats")]
+        [JsonConverter(typeof(Converters.StatisticsConverter))]
+        public AV.IUrlAnalysisStatistics Statistics { get; set; }
+        /// <summary>
+        /// if the origianl Url redirets, where does it end
+        /// </summary>
+        [JsonPropertyName("last_final_url")]
+        public string LastFinalUrl { get; set; }
+        /// <summary>
+        /// HTTP response code of the content received
+        /// </summary>
+        [JsonPropertyName("last_http_response_code")]
+        public int LastHttpResponseCode { get; set; }
+        /// <summary>
+        /// Length in bytes of the content recieved
+        /// </summary>
+        [JsonPropertyName("last_http_response_content_length")]
+        public int LastHttpResponseContentLength { get; set; }
+        /// <summary>
+        /// Url response body's SHA256 hash
+        /// </summary>
+        [JsonPropertyName("last_http_response_content_sha256")]
+        public string LastHttpResponseContentSha256 { get; set; }
+        /// <summary>
+        /// Contains the website's cookies
+        /// </summary>
+        [JsonPropertyName("last_http_response_cookies")]
+        public Dictionary<string, string> LastHttpResponseCookies { get; set; }
+        /// <summary>
+        /// Contains headers and values of last HTTP response
+        /// </summary>
+        [JsonPropertyName("last_http_response_headers")]
+        public Dictionary<string, string> LastHttpResponseHeaders { get; set; }
+        /// <summary>
+        /// Represents last modification date
+        /// </summary>
+        [JsonPropertyName("last_modification_date")]
+        [JsonConverter(typeof(Converters.DateTimeOffsetConverter))]
+        public DateTimeOffset LastModificationDate { get; set; }
+        /// <summary>
+        /// Represents the last time it was sent to be analysed
+        /// </summary>
+        [JsonPropertyName("last_submission_date")]
+        [JsonConverter(typeof(Converters.DateTimeOffsetConverter))]
+        public DateTimeOffset LastSubmissionDate { get; set; }
+        /// <summary>
+        /// Contains links to different domains
+        /// </summary>
+        [JsonPropertyName("outgoing_links")]
+        public List<string> OutgoingLinks { get; set; }
+        /// <summary>
+        /// Value of votes from VirusTotal community
+        /// </summary>
+        [JsonPropertyName("reputation")]
+        public int Reputation { get; set; }
+        /// <summary>
+        /// Tags
+        /// </summary>
+        [JsonPropertyName("tags")]
+        public List<string> Tags { get; set; }
+        /// <summary>
+        /// Targeted brand info extracted from phishing engines
+        /// </summary>
+        [JsonPropertyName("targeted_brand")]
+        public Dictionary<string, string> TargetedBrand { get; set; }
+        /// <summary>
+        /// Number of times that Url has been checked
+        /// </summary>
+        [JsonPropertyName("times_submitted")]
+        public int TimesSubmitted { get; set; }
+
+        /// <summary>
+        /// Webpage title
+        /// </summary>
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        /// <summary>
+        /// Contains votes from VirusTotal community
+        /// </summary>
+        [JsonPropertyName("total_votes")]
+        [JsonConverter(typeof(Converters.VotesConverter))]
+        public VT.IVotes TotalVotes { get; set; }
+
+        /// <summary>
+        /// Contains tracker information
+        /// </summary>
+        [JsonPropertyName("trackers")]
+        [JsonConverter(typeof(Converters.DictTrackers))]
+        public Dictionary<string, VT.ITracker[]> Trackers { get; set; }
+
+        /// <summary>
+        /// Original URL to be scanned
+        /// </summary>
+        [JsonPropertyName("url")]
+        public string OrignialUrl { get; set; }
+
+        IReadOnlyDictionary<string, string> IUrl.Categories => Categories;
+
+        IReadOnlyDictionary<string, string[]> IUrl.HtmlMeta => HtmlMeta;
+
+        IReadOnlyDictionary<string, IUrlAnalysisResult> IUrl.LastAnalysisResults => LastAnalysisResults;
+
+        IReadOnlyDictionary<string, string> IUrl.LastHttpResponseCookies => LastHttpResponseCookies;
+
+        IReadOnlyDictionary<string, string> IUrl.LastHttpResponseHeaders => LastHttpResponseHeaders;
+
+        IReadOnlyDictionary<string, string> IUrl.TargetedBrand => TargetedBrand;
+
+        IReadOnlyDictionary<string, ITracker[]> IUrl.Trackers => Trackers;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Url)) return false;
+            if (((Url)obj).OrignialUrl == OrignialUrl) return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return OrignialUrl;
+        }
+    }
+    /// <summary>
+    /// Extenction of the Url displaying premium features
+    /// </summary>
+    internal class PremiumUrl : Url, IPremiumUrl
+    {
+        /// <summary>
+        /// Includes difference hash and ms5 hash of the URLs favicon
+        /// </summary>
+        public Dictionary<IPremiumUrl.FaviconEnum, string> Favicon { get; set; }
+
+        IReadOnlyDictionary<IPremiumUrl.FaviconEnum, string> IPremiumUrl.Favicon => Favicon;
+    }
+}
