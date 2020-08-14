aa324678b635e436a7d35d276f234f362ac9f87b
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-08-14 18:08:55 +0200

    Added missing inheritance and method overrides

--- VirusTotalApi/VT/Tracker.cs
+++ VirusTotalApi/VT/Tracker.cs
@@ -5,7 +5,7 @@ namespace VirusTotalLib.VT
     /// <summary>
     /// Represents a site tracker
     /// </summary>
-    internal struct Tracker
+    internal struct Tracker : ITracker
     {
         /// <summary>
         /// Id of the tracker
@@ -18,6 +18,24 @@ namespace VirusTotalLib.VT
         /// </summary>
         [JsonPropertyName("url")]
         public string Url { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Tracker)) return false;
+            Tracker tracker = (Tracker)obj;
+            if (tracker.Id != Id
+                || tracker.Url != Url) return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
     /// <inheritdoc cref="Tracker"/>
     public interface ITracker
